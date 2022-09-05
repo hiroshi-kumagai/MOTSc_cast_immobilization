19791cd7581b66e1bf507e023225e823ecddc61f
emmamk <michitsuruta7@gmail.com>
2022-09-05 13:42:36 -0700

    removed unneccessary scripts

--- scripts/02.gtf2bed.sh
+++
@@ -1,8 +0,0 @@
-#!bin/sh
-# author: Emma Kumagai
-
-# change directory: reference genome
-cd /Users/Emma/Documents/Bioinformatics/RefGenome/ENSEMBLE/
-
-# converting gtf file to bed file
-gtf2bed < Homo_sapiens.GRCh38.104.gtf > Homo_sapiens.GRCh38.104.bed
